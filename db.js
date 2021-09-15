b64202480161baa8389b9121ff46330cd3a180fd
Mohammad Zafar <mzcoder7@gmail.com>
2021-09-15 19:25:50 +0100

    Check initial routes connect

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
-const Sequelize = require(“sequelize”);
-const db = new Sequelize(“eps”, “root”, “”, {
-  host: “localhost”,
-  dialect: “mysql”,
+const Sequelize = require("sequelize");
+const db = new Sequelize("eps", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
 });
+
 module.exports = db;
 global.db = db;
