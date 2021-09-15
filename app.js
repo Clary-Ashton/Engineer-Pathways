7950e155327829a6fc8b0955d13bf7ff6fb00de4
Clary Mawangi <78886789+Clary-Ashton@users.noreply.github.com>
2021-09-15 19:06:51 +0100

    added requirements and sequelize

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require(“express”);
+const exphbs = require(“express-handlebars”);
+const app = express();
+const port = process.env.PORT || 3000;
+const router = require(“./src/routes/index”);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(“public”));
+app.engine(“hbs”, exphbs({ extname: “.hbs” }));
+app.set(“view engine”, “hbs”);
+app.use(“/”, router);
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
